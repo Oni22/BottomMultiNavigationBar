38af6960c82aeecfc5780c291b5d15b08c4f72b6
Onur Özkan <onurozkan@Onurs-MBP.fritz.box>
2020-02-09 19:05:12 +0100

    flutter intent added

--- lib/bottommultinavigationbar.dart
+++ lib/bottommultinavigationbar.dart
@@ -1,5 +1,5 @@
 library bottommultinavigationbar;
 export 'package:bottommultinavigationbar/src/bottom_multi_navigation_bar.dart';
 export 'package:bottommultinavigationbar/src/page_navigator.dart';
-
+export 'package:bottommultinavigationbar/src/flutter_intent.dart';
 
