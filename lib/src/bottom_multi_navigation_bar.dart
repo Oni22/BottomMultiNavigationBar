7fd613ca54375199e780396e4acdc707f87aa8bb
Onur Özkan <onurozkan@Onurs-MBP.fritz.box>
2020-02-09 13:14:04 +0100

    added more attributes

--- lib/src/bottom_multi_navigation_bar.dart
+++ lib/src/bottom_multi_navigation_bar.dart
@@ -7,11 +7,42 @@ class BottomMultiNavigationBar extends StatefulWidget {
   final List<PageNavigator> pageNavigators;
   final int index;
   final Function(int index) onIndexChanged;
+  final Color selectedItemColor;
+  final Color unselectedItemColor;
+  final Color backgroundColor;
+  final double selectedFontSize;
+  final double unselectedFontSize;
+  final TextStyle selectedLabelStyle;
+  final IconThemeData selectedIconTheme;
+  final bool showSelectedLabels;
+  final bool showUnselectedLabels;
+  final Color fixedColor;
+  final double elevation;
+  final double iconSize;
+  final IconThemeData unselectedIconTheme;
+  final TextStyle unselectedLabelStyle;
+  final BottomNavigationBarType type;
   
   BottomMultiNavigationBar({
+    Key key,
     @required this.pageNavigators,
     @required this.index,
-    this.onIndexChanged
+    this.onIndexChanged,
+    this.selectedItemColor,
+    this.unselectedItemColor,
+    this.backgroundColor,
+    this.selectedFontSize,
+    this.elevation,
+    this.fixedColor,
+    this.iconSize,
+    this.selectedIconTheme,
+    this.selectedLabelStyle,
+    this.showSelectedLabels,
+    this.showUnselectedLabels,
+    this.type,
+    this.unselectedFontSize,
+    this.unselectedIconTheme,
+    this.unselectedLabelStyle
     }) : super();
 
   @override
@@ -25,19 +56,21 @@ class _BottomMultiNavigationBarState extends State<BottomMultiNavigationBar> {
   @override
   void initState() {
     super.initState();
-    setUpNavigationBar();
+    _setUpNavigationBar();
   }
 
-  void setUpNavigationBar() {
+  void _setUpNavigationBar() {
     for(int i = 0; i < widget.pageNavigators.length;i++) {
         _bottomNavigationBarItems.add(BottomNavigationBarItem(
+          backgroundColor: widget.pageNavigators[i].backgroundColor,
+          activeIcon: widget.pageNavigators[i].activeIcon,
           title: Text(widget.pageNavigators[i].bottomNavigationText),
           icon: widget.pageNavigators[i].bottomNavigationIcon
         ));
     }
   }
 
-  Widget buildOffstageNavigation(int index) {
+  Widget _buildOffstageNavigation(int index) {
     return Offstage(
       offstage: widget.index != index,
       child: Navigator(
@@ -56,17 +89,30 @@ class _BottomMultiNavigationBarState extends State<BottomMultiNavigationBar> {
         body: Stack(
           children: widget.pageNavigators.map((page) {
               int index = widget.pageNavigators.indexOf(page);
-              return buildOffstageNavigation(index);
+              return _buildOffstageNavigation(index);
           }).toList()
         ),
         bottomNavigationBar: Theme(
           data: Theme.of(context).copyWith(
-            canvasColor: Colors.white
+            canvasColor: widget.backgroundColor
           ),
           child: BottomNavigationBar(
-            //selectedItemColor: VidlyCorp.primaryColor,
-            //unselectedItemColor: VidlyCorp.darkMode,
+            selectedItemColor: widget.selectedItemColor,
+            unselectedItemColor: widget.unselectedItemColor,
             currentIndex: widget.index,
+            selectedFontSize: widget.selectedFontSize ?? 14.0,
+            selectedLabelStyle: widget.selectedLabelStyle,
+            selectedIconTheme: widget.selectedIconTheme,
+            showSelectedLabels: widget.showUnselectedLabels,
+            showUnselectedLabels: widget.showUnselectedLabels,
+            backgroundColor: widget.backgroundColor,
+            fixedColor: widget.fixedColor,
+            elevation: widget.elevation ?? 8.0,
+            iconSize: widget.iconSize ?? 24.0,
+            unselectedFontSize: widget.unselectedFontSize ?? 12.0,
+            unselectedIconTheme: widget.unselectedIconTheme,
+            unselectedLabelStyle: widget.unselectedLabelStyle,
+            type: widget.type ?? BottomNavigationBarType.fixed,
             onTap: (index) {
                 widget.onIndexChanged(index);
             },
--- lib/src/page_navigator.dart
+++ lib/src/page_navigator.dart
@@ -8,13 +8,17 @@ class PageNavigator {
     this.bottomNavigationIcon,
     this.bottomNavigationText,
     this.onGenerateRoute,
-    this.initialRoute
+    this.initialRoute,
+    this.backgroundColor,
+    this.activeIcon
   });
 
   Route<dynamic> Function(RouteSettings) onGenerateRoute;
   String initialRoute;
   Icon bottomNavigationIcon;
   String bottomNavigationText;
+  Color backgroundColor;
+  Widget activeIcon;
   GlobalKey<NavigatorState> key = GlobalKey<NavigatorState>();
 
 }
